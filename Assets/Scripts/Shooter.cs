f8762c370719ae9acf5881d79328a7e262ec5793
AminAllah <janipooraminallah@gmail.com>
2020-03-10 14:38:10 +0330

    First commit

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public int moveSpeed = 5;
+    public Rigidbody ball;
+    public int power = 2000;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(h, v, 0);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            //Debug.Log("Fire1was pressed!");
+
+            Rigidbody instance = Instantiate(ball, transform.position, transform.rotation) as Rigidbody;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            instance.AddForce(fwd * power);
+        }
+    }
+}
