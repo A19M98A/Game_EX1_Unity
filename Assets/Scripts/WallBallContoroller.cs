d5e2ec716d0e7a31d02260bd9fb4d66e7f637bb5
AminAllah <janipooraminallah@gmail.com>
2020-03-25 09:05:57 +0430

    fix sin WallBall

--- Assets/Scripts/WallBallContoroller.cs
+++ Assets/Scripts/WallBallContoroller.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class WallBallContoroller : MonoBehaviour
 {
+    public float Y = 1;
+    public float X = 1;
+    public float Z = 1;
+    public float maxY = 3;
+    public int N = 1;
+    public float sped = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Y = transform.position.y;
+        X = transform.position.x;
+        Z = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(X, Y, Z);
+        Y += N * sped;
+        if (Y > maxY)
+            N = -N;
+        if (Y < 1)
+            N = -N;
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 - [x] 3 Wall
 - [x] 2 Ball of wall
-- [ ] Script of sin wall
+- [x] Script of sin wall
 - [x] Script of Shoot
 - [ ] Wall of around
